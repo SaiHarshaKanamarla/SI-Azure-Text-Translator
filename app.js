f16735e9e51bcd63f26f574aa14a9116f9151367
sidharsha <siddharth.landers@gmail.com>
2020-11-28 13:24:46 -0500

    Transliterate without translation

--- app.js
+++ app.js
@@ -70,6 +70,35 @@ app.post('/api/detect',(req,res)=>{
     })
 })
 
+//Transliterate without translation
+app.post('/api/transliterate',(req,res)=>{
+    axios({
+        baseURL: endpoint,
+        url: '/transliterate',
+        method: 'post',
+        headers: {
+            'Ocp-Apim-Subscription-Key': subscriptionKey,
+            'Ocp-Apim-Subscription-Region': location,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': uuidv4().toString()
+        },
+        params: {
+            'api-version': '3.0',
+            'language': req.body.language,
+            'fromScript': req.body.fromScript,
+            'toScript': req.body.toScript
+        },
+        data: [{
+            'text': req.body.text
+        }],
+        responseType: 'json'
+    }).then(function(response){
+        console.log(JSON.stringify(response.data, null, 4));
+        res.send(JSON.stringify(response.data, null, 4))
+    }).catch((err)=>{
+        res.status(404).send(err);
+    })
+})
 
 
 app.listen(port);
